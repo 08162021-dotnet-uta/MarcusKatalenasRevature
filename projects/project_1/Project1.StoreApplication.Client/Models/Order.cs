0587063ca4c922b22289ecc19a241e1c737c9ae6
Bitazee <katalenasm2@mailbox.winthrop.edu>
2021-09-10 14:43:45 -0400

    Work towards p1

---
+++ projects/project_1/Project1.StoreApplication.Client/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace project1app.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderProducts = new HashSet<OrderProduct>();
+        }
+
+        public Guid OrderId { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid StoreId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal FinalPrice { get; set; }
+        public bool? Active { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Store Store { get; set; }
+        public virtual ICollection<OrderProduct> OrderProducts { get; set; }
+    }
+}
